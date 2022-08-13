72db5b87cc2c6442bd2d530512b6892c4e6c3434
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-08-13 20:12:01 +0500

    Completed shop project using SQL database

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,35 +1,34 @@
 const productModal = require('../modals/product');
 const cartModal = require('../modals/cart');
 
-// <--------------------- GET ALL PRODUCTS FROM CART --------------------> //
+// <--------------------- SHOW PRODUCT PAGE --------------------> //
 exports.getShop = (req, res, next) => productModal.viewProduct().
     then(([data, fieldData]) => res.render('shop/product-list', { prods: data, pageTitle: "Shop", path: '/products' })).catch(err => console.log(err));
 
+// <---------------------  SHOW INDEX PAGE --------------------> //
 exports.getIndex = (req, res) => productModal.viewProduct().
     then(([data, fieldData]) => res.render('shop/index', { prods: data, pageTitle: "Shop", path: '/' })).catch(err => console.log(err));
 
+// <--------------------- SHOW CART --------------------> //
 exports.getCart = (req, res) => cartModal.getCart().
     then(([data, fieldData]) => res.render('shop/cart', { products: data, pageTitle: "Your Cart", path: '/cart' })).catch(err => console.log(err));
 
+// <--------------------- SHOW CHECKOUT PAGE --------------------> //
 exports.getCheckout = (req, res) => res.render('shop/checkout', { pageTitle: "Checkout", path: "/checkout" });
 
+// <---------------------  --------------------> //
 exports.getOrders = (req, res) => res.render('shop/orders', { pageTitle: "Your Orders", path: "/orders" });
 
+// <--------------------- SHOW PRODUCT DETAIL PAGE --------------------> //
 exports.getProducts = (req, res) => {
     const prodId = req.params.productId;
     productModal.findById(prodId).then(([[data], fieldData]) => res.render('shop/product-detail', { prods: data, pageTitle: data.title, path: '/products' }));
 };
 
-exports.postCart = (req, res) => {
-    // const prodId = req.body.productId;
-    productModal.findById(req.body.productId);
-    res.redirect('/cart');
-};
+// <--------------------- ADD A PRODUCT TO CART (POST) --------------------> //
+exports.postCart = (req, res) => cartModal.addProduct(req.body.productId).
+    then(val => res.redirect('/cart')).catch(err => console.log(err));
 
-exports.postRemoveCartItem = (req, res, next) => {
-    const prodId = req.body.productId;
-    productModal.findById(prodId, product => {
-        cartModal.deleteCart(prodId, product.price);
-    });
-    res.redirect('/cart');
-};
+// <--------------------- REMOVE A PRODUCT FROM CART (POST) --------------------> //
+exports.postRemoveCartItem = (req, res, next) => cartModal.deleteCart(req.body.productId).
+    then(val => res.redirect('/cart')).catch(err => console.log(err));
--- modals/cart.js
+++ modals/cart.js
@@ -7,48 +7,16 @@ const path = require('path');
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json');
 
 // <--------------------- ADD A PRODUCT TO CART --------------------> //
-exports.addProduct = (productId) => {
-    return db.execute('SELECT * FROM cart WHERE productId = ', [productId]).then(([[data], fieldata]) => {
-        return console.log(data);
-    });
-    // fs.readFile(p, (err, data) => {
-    //     if (err) console.log(err);
-    //     const cart = JSON.parse(data);
-    //     if (cart?.products) {
-    //         const existingProductIndex = cart.products.findIndex(val => Number(val.id) === Number(productId));
-    //         const existingProduct = cart.products[existingProductIndex];
-    //         if (existingProduct) {
-    //             existingProduct.qty++;
-    //         } else {
-    //             cart.products.push({ id: productId, qty: 1 });
-    //         }
-    //         cart.totalPrice += Number(product.price);
-    //         fs.writeFile(p, JSON.stringify(cart), err => console.log(err));
-    //     }
-    //     else {
-    //         const productObject = { products: [{ id: product.id, qty: 1 }], totalPrice: Number(product.price) };
-    //         fs.writeFile(p, JSON.stringify(productObject), err => console.log(err));
-    //     }
-    // });
-};
+exports.addProduct = (productId) => db.execute('SELECT * FROM cart WHERE product_id = ?', [productId]).then(([[data], fieldata]) => {
+    if (data) db.execute('UPDATE cart SET qty = ? WHERE product_id = ?', [(data.qty + 1), data.product_id]).catch(err => console.log(err));
+    else db.execute('INSERT INTO cart (product_id, qty) VALUES (?, ?)', [productId, 1]).catch(err => console.log(err));
+});
 
 // <--------------------- REDUCE A PRODUCT FROM CART --------------------> //
-exports.deleteCart = (id, productPrice) => {
-    fs.readFile(p, (err, data) => {
-        if (err) console.log(err);
-        const cart = JSON.parse(data);
-        const filteredCart = cart.products.filter(val => Number(val.id) !== Number(id));
-        if (filteredCart.length !== cart.products.length) {
-            const indexOfdeletedProduct = cart.products.findIndex(val => Number(val.id) === Number(id));
-            const deletedProductQuantity = cart.products.filter(val => Number(val.id) === Number(id))[0].qty;
-            console.log(deletedProductQuantity);
-            if (Number(deletedProductQuantity) > 1) cart.products[indexOfdeletedProduct].qty -= 1;
-            else cart.products = filteredCart;
-            cart.totalPrice = Number(cart.totalPrice) - Number(productPrice);
-            fs.writeFile(p, JSON.stringify(cart), err => console.log(err));
-        }
-    });
-};
+exports.deleteCart = (id) => db.execute("SELECT * FROM cart WHERE product_id = ?", [id]).then(([[data], fieldData]) => {
+    if (data && data.qty > 1) db.execute("UPDATE cart SET qty = ? WHERE product_id = ?", [(data.qty - 1), data.product_id]).catch(err => console.log(err));
+    if (data && data.qty === 1) db.execute("DELETE FROM cart WHERE product_id = ?", [data.product_id]).catch(err => console.log(err));
+});
 
 // <--------------------- ELIMINATE THE PRODUCT FROM THE CART --------------------> //
 exports.deleteWholeProduct = (id) => db.execute('DELETE FROM cart WHERE id = ?', [id]);
